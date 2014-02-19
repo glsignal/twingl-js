7b4d97514d36dafc7b4a26511cfa0bf3ca09ef89
Greg Signal <greg@awesome.org.nz>
2014-02-19 13:50:50 +1300

    Remove whitespace

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@
   "use strict";
 
   window.Twingl = window.Twingl || {};
-  
+
   Twingl.Client = function (opts) {
     this.token    = opts.token;
     this.baseUrl  = opts.baseUrl || Twingl.defaults.baseUrl;
